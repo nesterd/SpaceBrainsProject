9707ce11779178e4958fd410683e0756288496c9
iridium174 <o_chizhov@mail.ru>
2017-01-21 01:49:12 +0500

    Revised main SQL script

--- Database/re_create_schema.sql
+++ Database/re_create_schema.sql
@@ -7,10 +7,17 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 -- -----------------------------------------------------
 -- Schema ratepersons
 -- -----------------------------------------------------
+-- База данных для проекта по сбору статистики упоминания личностей в сети интернет
+-- Проект выполняется в рамках программы стажировки в компании geekbrains.ru
+-- Группа программистов SpaceBrains
 DROP SCHEMA IF EXISTS `ratepersons` ;
 
 -- -----------------------------------------------------
 -- Schema ratepersons
+--
+-- База данных для проекта по сбору статистики упоминания личностей в сети интернет
+-- Проект выполняется в рамках программы стажировки в компании geekbrains.ru
+-- Группа программистов SpaceBrains
 -- -----------------------------------------------------
 CREATE SCHEMA IF NOT EXISTS `ratepersons` DEFAULT CHARACTER SET utf8 ;
 USE `ratepersons` ;
@@ -22,14 +29,11 @@ DROP TABLE IF EXISTS `ratepersons`.`Persons` ;
 
 CREATE TABLE IF NOT EXISTS `ratepersons`.`Persons` (
   `ID` INT NOT NULL AUTO_INCREMENT,
-  `Name` VARCHAR(1024) NOT NULL,
+  `Name` NVARCHAR(2048) NULL,
   PRIMARY KEY (`ID`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
+ENGINE = InnoDB;
 
 CREATE UNIQUE INDEX `ID_UNIQUE` ON `ratepersons`.`Persons` (`ID` ASC);
-
-CREATE UNIQUE INDEX `Name_UNIQUE` ON `ratepersons`.`Persons` (`Name` ASC);
 
 
 -- -----------------------------------------------------
@@ -39,7 +43,7 @@ DROP TABLE IF EXISTS `ratepersons`.`Keywords` ;
 
 CREATE TABLE IF NOT EXISTS `ratepersons`.`Keywords` (
   `ID` INT NOT NULL AUTO_INCREMENT,
-  `Name` VARCHAR(1024) NOT NULL,
+  `Name` NVARCHAR(2048) NULL,
   `PersonID` INT NOT NULL,
   PRIMARY KEY (`ID`),
   CONSTRAINT `FK_persons_keywords`
@@ -47,10 +51,7 @@ CREATE TABLE IF NOT EXISTS `ratepersons`.`Keywords` (
     REFERENCES `ratepersons`.`Persons` (`ID`)
     ON DELETE CASCADE
     ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
-
-CREATE UNIQUE INDEX `Name_UNIQUE` ON `ratepersons`.`Keywords` (`Name` ASC);
+ENGINE = InnoDB;
 
 CREATE INDEX `FK_Persons_idx` ON `ratepersons`.`Keywords` (`PersonID` ASC);
 
@@ -62,12 +63,9 @@ DROP TABLE IF EXISTS `ratepersons`.`Sites` ;
 
 CREATE TABLE IF NOT EXISTS `ratepersons`.`Sites` (
   `ID` INT NOT NULL AUTO_INCREMENT,
-  `Name` VARCHAR(1024) NOT NULL,
+  `Name` NVARCHAR(2048) NOT NULL,
   PRIMARY KEY (`ID`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
-
-CREATE UNIQUE INDEX `Name_UNIQUE` ON `ratepersons`.`Sites` (`Name` ASC);
+ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
@@ -77,18 +75,17 @@ DROP TABLE IF EXISTS `ratepersons`.`Pages` ;
 
 CREATE TABLE IF NOT EXISTS `ratepersons`.`Pages` (
   `ID` INT NOT NULL AUTO_INCREMENT,
-  `Url` VARCHAR(2048) NOT NULL,
+  `Url` NVARCHAR(2048) NOT NULL,
   `SiteID` INT NOT NULL,
-  `FoundDateTime` DATETIME NULL DEFAULT NULL,
-  `LastScanDate` DATETIME NULL DEFAULT NULL,
+  `FoundDateTime` DATETIME NULL,
+  `LastScanDate` DATETIME NULL,
   PRIMARY KEY (`ID`),
   CONSTRAINT `FK_sites_pages`
     FOREIGN KEY (`ID`)
     REFERENCES `ratepersons`.`Sites` (`ID`)
-    ON DELETE CASCADE
+    ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
+ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
@@ -100,18 +97,17 @@ CREATE TABLE IF NOT EXISTS `ratepersons`.`PersonPageRank` (
   `PersonID` INT NOT NULL,
   `PageID` INT NOT NULL,
   `Rank` INT NOT NULL,
-  CONSTRAINT `FK_pages_personpagerank`
-    FOREIGN KEY (`PageID`)
-    REFERENCES `ratepersons`.`Pages` (`ID`)
-    ON DELETE CASCADE
-    ON UPDATE NO ACTION,
   CONSTRAINT `FK_persons_personpagerank`
     FOREIGN KEY (`PersonID`)
     REFERENCES `ratepersons`.`Persons` (`ID`)
-    ON DELETE CASCADE
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `FK_pages_personpagerank`
+    FOREIGN KEY (`PageID`)
+    REFERENCES `ratepersons`.`Pages` (`ID`)
+    ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
+ENGINE = InnoDB;
 
 CREATE INDEX `FK_persons_idx` ON `ratepersons`.`PersonPageRank` (`PersonID` ASC);
 
@@ -121,13 +117,3 @@ CREATE INDEX `FK_pages_idx` ON `ratepersons`.`PersonPageRank` (`PageID` ASC);
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
--- begin attached script 'script'
-use `ratepersons`;
-INSERT INTO `persons` (`Name`) VALUES ("Путин");
-INSERT INTO `keywords` (`Name`, `PersonID`) VALUES ('Путину', 1);
-INSERT INTO `keywords` (`Name`, `PersonID`) VALUES ('Путиным', 1);
-INSERT INTO `keywords` (`Name`, `PersonID`) VALUES ('Путине', 1);
-INSERT INTO `sites` (`Name`) VALUES ('lenta.ru');
-INSERT INTO `pages` (`Url`, `SiteID`) VALUES ('http://lenta.ru/robots.txt', 1);
-INSERT INTO `personpagerank` (`PersonID`, `PageID`, `Rank`) VALUES (1, 1, 10);
--- end attached script 'script'
