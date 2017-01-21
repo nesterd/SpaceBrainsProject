c585875c9e622785f3496053f3808ac9d7c71c17
nester <nesterd85@gmail.com>
2017-01-22 01:44:27 +0700

    добавил bin и obj в gitignore

---
+++ WebAI/WebAI/Models/Base/BaseModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAI.Models.Base
+{
+    public class BaseModel
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ WebAI/WebAI/Models/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAI.Models
+{
+    public class Person
+        :Base.BaseModel
+    {
+        public string Name { get; set; }
+    }
+}
