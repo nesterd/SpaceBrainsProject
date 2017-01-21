0b14b40eabbe8197e76135a0cd51b74c9eceb4e7
nester <nesterd85@gmail.com>
2017-01-21 23:53:12 +0700

    Создано простое представление web интерфейса администратора выводятся фейковые данные

---
+++ WebAI/WebAI/WebAI/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebAI.Models;
+
+namespace WebAI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Persons()
+        {
+            return View( new List<Person> { new Person { Id = 1, Name = "Путин В.В." }, new Person { Id = 2, Name = "Медведев Д.А." } });
+        }
+
+        public ActionResult KeyWords()
+        {
+         
+            return View( new List<KeyWord> { new KeyWord { Id = 1, Name = "Путина", PersonId = 1 }, new KeyWord { Id = 2, Name = "Путину", PersonId = 1 } });
+        }
+
+        public ActionResult Sites()
+        {
+            return View(new List<Site> { new Site { Id = 1, Name = "lenta.ru", Url = "lenta.ru" } });
+        }
+
+        
+    }
+}
---
+++ WebAI/WebAI/WebAI/Models/KeyWord.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAI.Models
+{
+    public class KeyWord
+        : Base.BaseModel
+    {
+        public string Name { get; set; }
+        public int PersonId { get; set; }
+    }
+}
