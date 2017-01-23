6b65f76b91aa552a76f14d220a375342e807847a
nester <nesterd85@gmail.com>
2017-01-23 21:04:51 +0700

    Добавил проект DataAccess где будет реализован репозиторий, пересобрал проект проверил зависимости

---
+++ WebAI/DataAccess/Context/WebAIDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Context
+{
+    public class WebAIDbContext
+        : DbContext
+    {
+        public WebAIDbContext()
+            : base("NameOfDb")
+        {
+
+        }
+
+        public virtual DbSet<Person> Persons { get; set; }
+        public virtual DbSet<KeyWord> KeyWords { get; set; }
+        public virtual DbSet<Site> Sites { get; set; }
+        public virtual DbSet<Page> Pages { get; set; }
+        public virtual DbSet<PersonRangeRank> PersonRangeRanks { get; set; }
+    }
+}
