85a6be615b43a3516a95dec61fb13ddc700fec6c
nester <nesterd85@gmail.com>
2017-01-23 23:30:04 +0700

    Создан Репозиторий DataRepository(реализующий интерфейс IDataRepository) для работы с БД в нем реализованны след методы: -добавление -удаление -редактирование -получение списка сущностей

---
+++ WebAI/DataAccess/Repositories/Base/IDataRepository.cs
@@ -0,0 +1,29 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repositories.Base
+{
+    interface IDataRepository
+    {
+        void AddPerson(Person person);
+        void AddKeyWord(KeyWord keyWord);
+        void AddSite(Site site, string url);
+
+        void ChangePerson(Person person);
+        void ChangeKeyWord(KeyWord keyword);
+        void ChangeSite(Site site);
+
+        void DeleteKeyWord(KeyWord keyword);
+        void DeletePersonById(int id);
+        void DeleteSiteById(int id);
+
+        IEnumerable<Person> GetPersons();
+        IEnumerable<KeyWord> GetKeyWords();
+        IEnumerable<KeyWord> GetKeyWords(Person person);
+        IEnumerable<Site> GetSites();
+    }
+}
