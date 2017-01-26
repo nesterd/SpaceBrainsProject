12f5f413de53cfd967b935c92340055db2948238
nester <nesterd85@gmail.com>
2017-01-26 12:16:01 +0700

    добавил KeyWordDTO PersonDTO и businessLogicDependencyModule

---
+++ WebAI/BusinessLogic/DTO/KeyWordDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.DTO
+{
+    public class KeyWordDTO
+        : Base.BaseDTO
+    {
+        public string Name { get; set; }
+        public int PersonId { get; set; }
+    }
+}
---
+++ WebAI/BusinessLogic/DTO/PersonDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.DTO
+{
+    public class PersonDTO
+        : Base.BaseDTO
+    {
+        public string Name { get; set; }
+    }
+}
