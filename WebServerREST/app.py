7650f8024cd119a8d6035a6157f1ddba12cfd968
luckywookie <olga.belykh@gmail.com>
2017-01-27 20:16:26 +0300

    add requirements.txt and fix app

--- WebServerREST/app.py
+++ WebServerREST/app.py
@@ -3,7 +3,9 @@ from flask_restful import Api
 
 from resources.site import Site, SiteList
 from resources.person import Person, PersonList
-# from resources.pages import Page, PageList
+# from resources.pages import Page
+from models.pages import PageModel
+from resources.keyword import Keyword, KeywordList
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
@@ -21,6 +23,8 @@ api.add_resource(Site, '/site/<int:ID>', '/site/<string:Name>')
 api.add_resource(SiteList, '/sites')
 api.add_resource(Person, '/person/<string:Name>', '/person/<int:ID>')
 api.add_resource(PersonList, '/persons')
+api.add_resource(Keyword, '/keyword/<string:Name>', '/keyword/<int:ID>')
+api.add_resource(KeywordList, '/keywords')
 
 if __name__ == '__main__':
     from db import db
