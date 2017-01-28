7ee82ed2ab8d575852fb36746fa5a48fae6a8571
nester <nesterd85@gmail.com>
2017-01-28 14:19:26 +0700

    добавил для проверки в инициализатор БД страницы и данные в таблицу PersonPageRank

--- WebAI/DataAccess/Initializer/DbContextInitializer.cs
+++ WebAI/DataAccess/Initializer/DbContextInitializer.cs
@@ -38,9 +38,22 @@ namespace DataAccess.Initializer
             context.Pages.AddRange(new List<Page>
             {
                 new Page { Url = "lenta.ru", FoundDateTime = DateTime.Now, SiteId = 1 },
-                new Page { Url = "mail.ru", FoundDateTime = DateTime.Now, SiteId = 2 }
+                new Page { Url = "lenta.ru/1", FoundDateTime = DateTime.Now, SiteId = 1 },
+                new Page { Url = "lenta.ru/2", FoundDateTime = DateTime.Now, SiteId = 1 },
+                new Page { Url = "mail.ru", FoundDateTime = DateTime.Now, SiteId = 2 },
+                new Page { Url = "mail.ru/1", FoundDateTime = DateTime.Now, SiteId = 2 },
+                new Page { Url = "mail.ru/2", FoundDateTime = DateTime.Now, SiteId = 2 }
             });
             context.SaveChanges();
+
+            context.PersonPageRanks.AddRange(new List<PersonPageRank>()
+            {
+                new PersonPageRank { PageId = 2, PersonId =1, Rank = 10 },
+                new PersonPageRank { PageId = 3, PersonId = 1, Rank = 12 },
+                new PersonPageRank { PageId = 5, PersonId = 1, Rank = 7 },
+                new PersonPageRank { PageId = 2, PersonId = 2, Rank = 19 }
+            });
+                
         }
     }
 }
