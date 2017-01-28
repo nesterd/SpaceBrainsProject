3f6a8171e5a263988305999e9ae6e3bf0e3a75e6
ivn <ivn@li.ru>
2017-01-29 01:22:53 +0300

    статистика, требуется проверить запросы

--- WebServerREST/models/pages.py
+++ WebServerREST/models/pages.py
@@ -12,4 +12,17 @@ class PageModel(db.Model):
     SiteID = db.Column(db.Integer, db.ForeignKey('Sites.ID'))
     Site = db.relationship('SiteModel')
 
+    def json(self):
+        return {'site':                     SiteModel.query.filter_by(ID=self.SiteID).first(),
+                'total_count':              PageModel.query.filter_by(SiteID=self.SiteID).count(),
+                'total_count_not_round':    PageModel.query.filter_by(LastScanDate=None).count(),
+                'total_count_round':        PageModel.query.filter(PageModel.LastScanDate!=None).count()}
 
+    @classmethod
+    def find_by_id(cls, ID):
+        return cls.query.filter_by(SiteID=ID).first()
+
+    @classmethod
+    def find_by_name(cls, Name):
+        siteID = SiteModel.query.filter_by(Name=Name).first()
+        return cls.query.filter_by(SiteID=siteID).first()
