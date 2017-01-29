541568ac95bfd68d49822c69f1772cc4b10e6d2a
luckywookie <olga.belykh@gmail.com>
2017-01-29 17:39:20 +0300

    add security file and add user registration to app, and add JWT app

--- WebServerREST/app.py
+++ WebServerREST/app.py
@@ -1,11 +1,16 @@
 from flask import Flask
 from flask_restful import Api
+from flask_jwt import JWT
+
+from security import authenticate, identity
+from resources.user import UserRegister
 
 from resources.site import Site, SiteList
 from resources.person import Person, PersonList
-# from resources.pages import Page
+from resources.stats import Pages, StatList
 from models.pages import PageModel
 from resources.keyword import Keyword, KeywordList
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
@@ -18,6 +23,7 @@ api = Api(app)
 def create_tables():
     db.create_all()
 
+jwt = JWT(app, authenticate, identity)  # /auth
 
 api.add_resource(Site, '/site/<int:ID>', '/site/<string:Name>')
 api.add_resource(SiteList, '/sites')
@@ -25,8 +31,9 @@ api.add_resource(Person, '/person/<string:Name>', '/person/<int:ID>')
 api.add_resource(PersonList, '/persons')
 api.add_resource(Keyword, '/keyword/<string:Name>', '/keyword/<int:ID>')
 api.add_resource(KeywordList, '/keywords')
-api.add_resource(Stats, '/base_statistic/<string:Name>', '/base_statistic/<int:ID>')
+api.add_resource(StatList, '/base_statistic')
 
+api.add_resource(UserRegister, '/register')
 
 if __name__ == '__main__':
     from db import db
