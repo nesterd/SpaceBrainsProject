de8da55bb1fd25ed5784d96dd69cf7e663bf032f
luckywookie <olga.belykh@gmail.com>
2017-01-29 17:35:32 +0300

    add model for user and fix some errors of pages

--- WebServerREST/models/pages.py
+++ WebServerREST/models/pages.py
@@ -13,7 +13,7 @@ class PageModel(db.Model):
     Site = db.relationship('SiteModel')
 
     def json(self):
-        return {'site':                     SiteModel.query.filter_by(ID=self.SiteID).first(),
+        return {'site':  self.SiteID,                  # SiteModel.query.filter_by(ID=self.SiteID).first(),
                 'total_count':              PageModel.query.filter_by(SiteID=self.SiteID).count(),
                 'total_count_not_round':    PageModel.query.filter_by(LastScanDate=None).count(),
                 'total_count_round':        PageModel.query.filter(PageModel.LastScanDate!=None).count()}
@@ -22,7 +22,7 @@ class PageModel(db.Model):
     def find_by_id(cls, ID):
         return cls.query.filter_by(SiteID=ID).first()
 
-    @classmethod
-    def find_by_name(cls, Name):
-        siteID = SiteModel.query.filter_by(Name=Name).first()
-        return cls.query.filter_by(SiteID=siteID).first()
+ #   @classmethod
+ #   def find_by_name(cls, Name):
+        # siteID = SiteModel.query.filter_by(Name=Name).first()
+ #       return cls.query.filter_by(SiteID=siteID).first()
