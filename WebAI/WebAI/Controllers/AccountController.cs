9d760139a5d3c7e79d447761707b101cdbe81e7f
nester <nesterd85@gmail.com>
2017-01-30 10:10:16 +0700

    создан AccountController

---
+++ WebAI/WebAI/Controllers/AccountController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebAI.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public ActionResult LogIn()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult LogIn()
+        {
+            return View();
+        }
+    }
+}
