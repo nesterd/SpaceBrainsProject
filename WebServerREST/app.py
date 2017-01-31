97401d529543915c89467671e1d80f5c21b98465
ivn <ivn@li.ru>
2017-01-31 15:52:17 +0300

    пробный - рабочий вариант с сервера + первая рабочая статистика

--- WebServerREST/app.py
+++ WebServerREST/app.py
@@ -7,17 +7,19 @@ from resources.user import UserRegister
 
 from resources.site import Site, SiteList
 from resources.person import Person, PersonList
-from resources.stats import Pages, StatList
+from resources.stats import Pages as Stats, StatList
 from models.pages import PageModel
 from resources.keyword import Keyword, KeywordList
 
+import logging
+from logging.handlers import RotatingFileHandler
+
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://oldfox:StrongPassword111@93.174.131.56:3306/ratepersons'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = ''
 api = Api(app)
-
 
 @app.before_first_request
 def create_tables():
@@ -31,11 +33,26 @@ api.add_resource(Person, '/person/<string:Name>', '/person/<int:ID>')
 api.add_resource(PersonList, '/persons')
 api.add_resource(Keyword, '/keyword/<string:Name>', '/keyword/<int:ID>')
 api.add_resource(KeywordList, '/keywords')
-api.add_resource(StatList, '/base_statistic')
+api.add_resource(Stats, '/base_statistic/<int:ID>', '/base_statistic/<string:Name>') #Stats
 
 api.add_resource(UserRegister, '/register')
 
 if __name__ == '__main__':
+
+    log = logging.getLogger(__name__)
+    log.setLevel(logging.DEBUG)
+    # add a file handler
+    fh = logging.handlers.TimedRotatingFileHandler("log/app_log.log",when='M',interval=86400,backupCount=0)
+    fh.setLevel(logging.DEBUG)
+    # create a formatter and set the formatter for the handler.
+    frmt = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    fh.setFormatter(frmt)
+    # add the Handler to the logger
+    log.addHandler(fh)
+
+    logging.getLogger('werkzeug').addHandler(fh)
+    
     from db import db
     db.init_app(app)
-    app.run(port=5000, debug=True)
+    #app.run(port=5000, debug=True)
+    app.run(host='93.174.131.56', debug=True)
